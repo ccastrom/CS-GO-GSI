102e8bc91151cfd8b183aa4d0a82ccedd4742776
BRAVOSIX <claudio_jr@live.cl>
2021-09-05 13:15:36 -0300

    Se agregan archivos a la carpeta

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="resultado.php" method="get">
+        <labe>set an ID</labe>
+        <input type="text" name="idUser" id="idUser">
+        <br>
+        <input type="submit" name="btnEnviar" value="enviar">
+       
+    </form>
+    <?php
+        session_start();
+            if(isset($_SESSION["nombre"])){
+             $nombre=$_SESSION["nombre"];
+             echo $nombre;
+            }else{
+               echo "not ok";
+            }
+
+        ?>
+</body>
+</html>
