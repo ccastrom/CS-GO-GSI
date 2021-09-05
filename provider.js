07e7283acece827772249cc2fccde9570f1a1f57
BRAVOSIX <claudio_jr@live.cl>
2021-09-05 19:04:08 -0300

    Se corrije error al mostrar por consola

--- provider.js
+++ provider.js
@@ -1,7 +1,6 @@
 function provider(JSON){
     var steamID=JSON.provider.steamid;
-    console.log(name);
-    console.log(steamID);
+    console.log("User ID"+steamID);
 }
    
 module.exports.provider=provider;
